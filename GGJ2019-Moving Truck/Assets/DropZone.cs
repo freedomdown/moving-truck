d6a35fddf6fae56dc48977bb0a651dadc3a56833
Ryan Dallaire <ryan.w.dallaire@gmail.com>
2019-01-26 13:50:31 -0700

    dragging is fixed

--- GGJ2019-Moving Truck/Assets/DropZone.cs
+++ GGJ2019-Moving Truck/Assets/DropZone.cs
@@ -7,22 +7,22 @@ public class DropZone : MonoBehaviour
     public GameObject Display;//obj gets enabled to show feedback before dropping
     public DragAndDrop draggingArea;//parent to tell when safetodrop/trash
     public bool Trash = false;//this drop zone is for the trash, not the truck
-    public bool test = false;
 
     void OnMouseEnter()
     {
-        test = true;
-        if (!Trash)
-            draggingArea.SafeToDrop = true;
-        else
-            draggingArea.SafeToTrash = true;
-        if (Display != null)
-            Display.SetActive(true);
+        if (draggingArea.SelectedObject != null)
+        {
+            if (!Trash)
+                draggingArea.SafeToDrop = true;
+            else
+                draggingArea.SafeToTrash = true;
+            if (Display != null)
+                Display.SetActive(true);
+        }
     }
 
     void OnMouseExit()
     {
-        test = false;
         if (!Trash)
             draggingArea.SafeToDrop = false;
         else
