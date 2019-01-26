fed38eae600bf5a5904c6ade26e3e92866ef7de1
Ryan Dallaire <ryan.w.dallaire@gmail.com>
2019-01-26 12:26:09 -0700

    dragging and dropping

--- GGJ2019-Moving Truck/Assets/DragAndDrop.cs
+++ GGJ2019-Moving Truck/Assets/DragAndDrop.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour
 {
-    public Transform SelectedObject;
-    public float GridSpacing = 1f;
-    public int LeftLimit = -1;
-    public int RightLimit = 1;
+    public Transform SelectedObject;//the object itself
+    public int ObjectWidth = 1;//the grid width of the object
+    public float GridSpacing = 1f;//how wide is a grid space
+    public int LeftLimit = -1;//x coord left most side of grid
+    public int RightLimit = 1;//y coord right most side of grid
+
+    public bool SafeToDrop = false;//is it safe to drop an item
+    public bool SafeToTrash = false;
     
     void OnMouseDrag()
     {
@@ -15,13 +19,34 @@ public class DragAndDrop : MonoBehaviour
         {
             Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
             Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+            curPosition = new Vector3(curPosition.x, curPosition.y, 0f);
 
-            int gridNum = (int)(curPosition.x / GridSpacing);
-            if (gridNum < LeftLimit)
-                gridNum = LeftLimit;
-            else if (gridNum > RightLimit)
-                gridNum = RightLimit;
-            curPosition = new Vector3(gridNum * GridSpacing, curPosition.y, 0f);
+            if (SafeToDrop)
+            {
+                int gridNum = (int)(curPosition.x / GridSpacing);
+                switch (ObjectWidth)
+                {
+                    case 1:
+                        if (gridNum < LeftLimit)
+                            gridNum = LeftLimit;
+                        else if (gridNum > RightLimit)
+                            gridNum = RightLimit;
+                        break;
+                    case 2:
+                        if (gridNum < LeftLimit)
+                            gridNum = LeftLimit;
+                        else if (gridNum > RightLimit - 1)
+                            gridNum = RightLimit - 1;
+                        break;
+                    case 3:
+                        if (gridNum < LeftLimit)
+                            gridNum = LeftLimit;
+                        else if (gridNum > RightLimit - 2)
+                            gridNum = RightLimit - 2;
+                        break;
+                }
+                curPosition = new Vector3(gridNum * GridSpacing, 3f, 0f);
+            }
 
             SelectedObject.position = curPosition;
         }
@@ -29,10 +54,27 @@ public class DragAndDrop : MonoBehaviour
 
     void OnMouseUp()
     {
-        if (SelectedObject != null)
+        if (SafeToDrop)
         {
-            SelectedObject.SendMessage("Fall");
-            SelectedObject = null;
+            if (SelectedObject != null)
+            {
+                SelectedObject.SendMessage("Fall");//tell object to fall
+                SelectedObject = null;//is no longer selected
+            }
+        }
+        else if (SafeToTrash)
+        {
+            if (SelectedObject != null)
+            {
+                GameObject.Destroy(SelectedObject);
+            }
+        }
+        else
+        {
+            //do something other than drop it
+            //Back to selection thing?
+            if (SelectedObject != null)
+                SelectedObject.position = new Vector3(4f, 4f, 0f);
         }
     }
 
---
+++ GGJ2019-Moving Truck/Assets/DropZone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropZone : MonoBehaviour
+{
+    public GameObject Display;//obj gets enabled to show feedback before dropping
+    public DragAndDrop draggingArea;//parent to tell when safetodrop/trash
+    public bool Trash = false;//this drop zone is for the trash, not the truck
+
+    void OnMouseEnter()
+    {
+        if (!Trash)
+            draggingArea.SafeToDrop = true;
+        else
+            draggingArea.SafeToTrash = true;
+        if (Display != null)
+            Display.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        if (!Trash)
+            draggingArea.SafeToDrop = false;
+        else
+            draggingArea.SafeToTrash = false;
+        if (Display != null)
+            Display.SetActive(false);
+    }
+}
