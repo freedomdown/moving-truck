99510a26b220983b1f0133a93a361a93a6b35ff0
Pellanor <pellanor@gmail.com>
2019-01-27 01:08:14 -0700

    Juicy Destruction

--- GGJ2019-Moving Truck/Assets/Durability.cs
+++ GGJ2019-Moving Truck/Assets/Durability.cs
@@ -7,15 +7,28 @@ public class Durability : MonoBehaviour
 {
     public float Strength = 4f;
     public float Weight = 1f;
+    public float DestructionMagnitutde = 0.02f;
     public AudioSource breakSnd;
+    private bool breaking = false;
 
 
     public void Break()
     {
         //too much weight
-        //Destroy self?
-        Destroy(gameObject);
-        Debug.Log("Break!");
-        breakSnd.Play();
+        if (!breaking) {
+            //Destroy self?
+            breakSnd.Play();
+            breaking = true;
+            Destroy(gameObject, breakSnd.clip.length);
+        }
+    }
+
+    private void Update() {
+        if (breaking) {
+            SpriteRenderer sprite = gameObject.GetComponentInChildren<SpriteRenderer>();
+            Vector3 offset = Random.insideUnitCircle * DestructionMagnitutde;
+            sprite.transform.position = sprite.transform.position + offset;
+            sprite.transform.rotation = Quaternion.Slerp(sprite.transform.rotation, Random.rotation, DestructionMagnitutde);
+        }
     }
 }
