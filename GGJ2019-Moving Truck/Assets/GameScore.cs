cfe24b0fb8873014eba7fadb226ec4154b77d459
Pellanor <pellanor@gmail.com>
2019-01-27 11:06:07 -0700

    Calculate Score on the fly

---
+++ GGJ2019-Moving Truck/Assets/GameScore.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScore : MonoBehaviour
+{
+    public Transform ItemsGroup;
+    public Text utilityScore;
+    public Text joyScore;
+    private int currentUtility = 0;
+    private int currentJoy = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ItemScore[] scores = ItemsGroup.GetComponentsInChildren<ItemScore>();
+        int utility = scores.Sum(score => score.GetUtility());
+        int joy = scores.Sum(score => score.GetJoy());
+        if (currentUtility < utility) {
+            currentUtility++;
+        } else if (currentUtility > utility) {
+            currentUtility--;
+        }
+        if (currentJoy < joy) {
+            currentJoy++;
+        } else if (currentJoy > joy) {
+            currentJoy--;
+        }
+        joyScore.text = "Joy\n" + currentJoy;
+        utilityScore.text = "Utility\n" + currentUtility;
+    }
+}
--- GGJ2019-Moving Truck/Assets/ItemScore.cs
+++ GGJ2019-Moving Truck/Assets/ItemScore.cs
@@ -10,10 +10,17 @@ public class ItemScore : MonoBehaviour
     public int Utility;
     public State state = State.InQueue;
     
-    public double GetScore() {
+    public int GetJoy() {
         if (state == State.OnTruck)
         {
-            return Joy; //todo: maths here
+            return Joy;
+        }
+        return 0;
+    }
+
+    public int GetUtility() {
+        if (state == State.OnTruck) {
+            return Utility;
         }
         return 0;
     }
