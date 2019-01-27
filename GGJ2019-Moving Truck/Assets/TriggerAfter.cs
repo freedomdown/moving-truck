baa398352765b01787d4c7e9ccbe29f086004f06
Ryan Dallaire <ryan.w.dallaire@gmail.com>
2019-01-27 15:14:19 -0700

    insctructions

---
+++ GGJ2019-Moving Truck/Assets/TriggerAfter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace RyanDStandard
+{
+    namespace Events
+    {
+        public class TriggerAfter : MonoBehaviour
+        {
+            public float Delay = 1f;
+            private float spawnTime = 0f;
+            public UnityEvent triggerEvent;
+
+            // Use this for initialization
+            void Start()
+            {
+                spawnTime = Time.time;
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+                if (Time.time > spawnTime + Delay)
+                {
+                    triggerEvent.Invoke();
+                }
+            }
+        }
+    }
+}
