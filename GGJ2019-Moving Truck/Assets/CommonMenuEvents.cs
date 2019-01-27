73c49cf19fc0c2db76bb430b1a63c0cac1683085
Ryan Dallaire <ryan.w.dallaire@gmail.com>
2019-01-26 23:33:25 -0700

    MainMenu

---
+++ GGJ2019-Moving Truck/Assets/CommonMenuEvents.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RyanDStandard
+{
+    namespace Events
+    {
+        public class CommonMenuEvents : MonoBehaviour
+        {
+            [Header("public functions you can call from other scripts")]
+            public float Timeout = 2f;
+
+            public void SwitchScenesNumber(int SceneNumber)
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(SceneNumber);
+            }
+            public void SwitchScenes(string SceneName)
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(SceneName);
+            }
+            public void QuitApplication()
+            {
+                Application.Quit();
+            }
+            public void EnableObject(GameObject obj)
+            {
+                obj.SetActive(true);
+            }
+            public void WaitThenEnableObject(GameObject obj)
+            {
+                StartCoroutine(TimerDone(obj));
+            }
+
+            private IEnumerator TimerDone(GameObject obj)
+            {
+                yield return new WaitForSeconds(Timeout);
+                EnableObject(obj);
+            }
+        }
+    }
+}
